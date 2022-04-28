2730320e5cdaf30da965adabacaec439bf0ee904
mackenzie <macmcclemont@hotmail.com>
2022-04-28 17:29:28 -0400

    added play again button on game over componenent

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,12 +1,16 @@
 import IndividualQuestion from "./individualQuestion";
 
 function GameOver(props) {
+
+    const handleRestart = () => {
+        window.location.reload()
+    }
     return (
         <div className="gameOver">
             <div className="questionCard">
                 <h2>Game Over!</h2>
-                <h3>Your Score: {props.displayScore}</h3>
-                <h4>{props.displayQuestion}</h4>
+                <h3>Your Score: <span className="scoreInt">{props.displayScore}</span></h3>
+                <button className="restartButton" onClick={handleRestart}>Play Again!</button>
             </div>          
         </div>
     )
